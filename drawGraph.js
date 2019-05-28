2f194b08ac3bea2b4317604821ded9e4a234bc05
Mustafa Siddiqui <mustafa.ar.siddiqui@gmail.com>
2019-05-28 11:26:11 +0530

    Updated version - pre lunch

--- drawGraph.js
+++ drawGraph.js
@@ -2,17 +2,19 @@ drawGraphNodes = () => {
     let container = document.createElement('div')
     container.id = 'container';
     let gridNumber = 1
-    let widthOfParent = container.clientWidth
-    let heightOfParent = container.clientHeight
+    let widthOfParent = document.container.clientWidth
+    let heightOfParent = document.container.clientHeight
     let xDirection = widthOfParent / 20
     let yDirection = heightOfParent / 20
-    for (let indexY = 0; indexY < yDirection; indexY++) {
-        for (let indexX = 0; indexX < xDirection; indexX++) {
+    for (let indexY = 0; indexY < 100; indexY++) {
+        for (let indexX = 0; indexX < 100; indexX++) {
             let div = document.createElement('div');
             container.appendChild(div);
             div.style.width = `20px`
             div.id = `${gridNumber++}`
-            div.style.display = "inline-block"
+            div.style.position= "absolute"
+            div.style.top = `${indexY*20}`
+            div.style.left = `${indexX*20}`
         }
     } 
 }
