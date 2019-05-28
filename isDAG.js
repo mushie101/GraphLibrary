d7ee8523d86f2a2a03c9e695fd473c498756e592
Shubhi Dutta <shubhangi.dutta@research.iiit.ac.in>
2019-05-28 11:00:46 +0530

    some cleanup + incomplete code

--- isDAG.js
+++ isDAG.js
@@ -1,6 +1,26 @@
-//only check if isDir is true
-isDAG = (mat) => {
+isDAG = (mat, num) => {
+for (var i = 0; i < num; i++){
+    let recurStack = []
+
+    }
+}
 
 
-    
+DFS = (source, mat, num) => {
+let visited=[]
+for (var i = 0; i < num; i++) 
+    visited[i] = false; 
+DFSRecur(source, visited, mat)
+}
+
+DFSRecur = (vert, visited, mat) => { 
+    visited[vert] = true; 
+    console.log(vert);   
+    for (var i = 0; i< num; i++) { 
+        if (mat[vert][i]==1){
+        let elem = i; 
+        if (!visited[elem]) 
+            DFSRecur(elem, visited); 
+        } 
+    } 
 }
