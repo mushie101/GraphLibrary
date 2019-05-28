62275f3f1c5bfff8b7c8d44d477f353970da5706
Shubhi Dutta <shubhangi.dutta@research.iiit.ac.in>
2019-05-28 11:39:44 +0530

    added delAll and delNode(lazy)

--- adjMatrix.js
+++ adjMatrix.js
@@ -7,18 +7,35 @@ for (var i = 0; i < mat.length; i++) {
 return mat
 }
 
-addEdgeMat = (source, target, isDir, mat) => {
+addEdgeMat = (source, target, isDirected, mat) => {
 mat[source][target]=1
-if (isDir==0){
+if (isDirected==0){
     mat[target][source]=1
     }
 
 }
 
-delEdgeMat = (source, target, isDir, mat) => {
+delEdgeMat = (source, target, isDirected, mat) => {
 mat[source][target]=0
-if (isDir==0){
+if (isDirected==0){
     mat[target][source]=0
     }
 }
 
+//addNodeMat = (mat, num) => {
+
+
+//}
+
+//Lazy deletion
+delNodeMat = (mat, num, node) => {
+let edgeList = new Array(num).fill(-1)
+mat[node]=edgeList
+for (var i = 0; i < num; i++){
+    mat[i][node]=-1
+    }
+}
+
+delAllNodesMat = (mat, num) => {
+mat=adjMat(num)
+}
