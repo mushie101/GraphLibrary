9bf02c7fadb2dea244737b72e04f8af122fef77b
Shubhi Dutta <shubhangi.dutta@research.iiit.ac.in>
2019-05-30 10:17:18 +0530

    fixed + tested printing DAG

--- isDAG.js
+++ isDAG.js
@@ -9,20 +9,20 @@ DFS = (source, mat, num) => {
 let visited=[]
 for (let i = 0; i < num; i++) 
     visited[i] = false; 
-DFSRecur(source, visited, mat, chk)
+DFSRecur(source, visited, mat, num)
 }
 
-DFSRecur = (vert, visited, mat) => { 
+DFSRecur = (vert, visited, mat, num) => { 
     visited[vert] = true; 
     console.log(vert);   
     for (let i = 0; i< num; i++) { 
         if (mat[vert][i]==1){
         let elem = i; 
         if (!visited[elem]) 
-            DFSRecur(elem, visited, mat, ); 
+            DFSRecur(elem, visited, mat, num); 
         else{
             console.log("DAG")
-            break
+            return 1
         }
             
         } 
