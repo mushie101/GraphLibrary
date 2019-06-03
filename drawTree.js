04705c9910f5166a3df5d75e5cab7650932b95f6
Mustafa Siddiqui <mustafa.ar.siddiqui@gmail.com>
2019-06-03 13:05:08 +0530

    Added updated randomise function

--- drawTree.js
+++ drawTree.js
@@ -2,53 +2,45 @@ let counter = 0
 /* Takes as input the array size and max value of the elements (100 by default)
 Randomly generates a array of given size and max values.
 Returns array*/
-
-randomHeapGenerator = (num, maxVal=100) => {
-    let heap = new Array()
-    for (let i = 0; i < num;){
+randomHeapGenerator = (num) => {
+    let maxVal = 100;
+    let heap = new Array();
+    for (let i = 0; i < num; i++){
         heap[i] = Math.floor((Math.random() * maxVal));
-        let chk=0 
-        for (let j = 0; j < i; j++){
-            if (heap[i]!=heap[j]){
-                chk++
-                }
-            }
-        if (chk==i){
-            i++
-            }
-        }
-    for (let i = ((num/2)-1); i >= 0; i--){ 
-        heapify(heap, num, i)
-        }
-    drawFromArray(heap)
+    }
+    for(let i = Math.floor(num/2); i>=1; i--){
+        heapify(heap, num, i);
+    }
+    return heap;   
+    }
     /* Heapifies passed array recursively */
-}
-    heapify = (array, num, max) => {
-        var largest = max;
-        var l = 2*max + 1; 
-        var r = 2*max + 2; 
-        if (l < num && array[l] > array[largest]) 
-            largest = l; 
-          
-        if (r < num && array[r] > array[largest]) 
+heapify = (array, num, max) => {
+    var largest = max;
+    let l = 2 * max + 1; // left child 
+    let r = 2 * max + 2; // right child   
+        // If left child is larger than root 
+    if (l < num && array[l] > array[largest]) 
+        largest = l; 
+        // If right child is larger than max so far 
+    if (r < num && array[r] > array[largest]) 
             largest = r; 
-        
-        if (largest != max) 
-        { 
-            let temp = array[max]
-            array[max]=array[largest]
-            array[largest]=temp 
-            heapify(array, num, largest); 
-        } 
+        // If max is not root 
+    if (largest != max) { 
+        var temp = array[max];
+        array[max] = array[largest];
+        array[largest] = temp;
+            // Recursively heapify the affected sub-tree 
+        heapify(array, num, largest); 
     } 
-    
-drawFromArray = (array) => {
+} 
+    //console.log(randomHeapGenerator(5,15))   
+makeTreeFromArray = (array) => {
     counter = 0
     for(let index = 0; index < array.length; index++){
         drawTreeNodes()
         makeCanvasTree()
     }
-}
+} 
 makeTree = () => {
     console.log(counter)
     let tree = document.getElementById(`${counter}`)
